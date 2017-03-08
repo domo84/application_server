1f9cda5bd8513b6213d94f12128e1fed6548b4d1
Magnus Hellman <magnus.hellman@gmail.com>
2017-03-08 13:38:39 +0100

    WIP

--- workers/reload.js
+++ workers/reload.js
@@ -1,8 +1,11 @@
 "use strict";
 
+const Environment = require("../lib/environment");
 const socket = require("socket.io")();
 
-socket.listen(8001);
+let env = new Environment();
+
+socket.listen(env.config.application_server.reload);
 
 socket.on("connection", function(socket)
 {
