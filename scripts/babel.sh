867a0db81a0d0e449e2c8d6b42979d36974a559a
Magnus Hellman <magnus.hellman@gmail.com>
2017-03-13 17:27:37 +0100

    WIP

--- scripts/babel.sh
+++ scripts/babel.sh
@@ -3,4 +3,4 @@
 bin=$(readlink -f ./node_modules/.bin/browserify)
 preset=$(readlink -f ./node_modules/babel-preset-env)
 
-$bin $@ -t [ babelify --presets [ $preset ] ] --no-bundle-external
+$bin $@ -t node-underscorify -t jstify -t [ babelify --presets [ $preset ] ] --no-bundle-external --fast
