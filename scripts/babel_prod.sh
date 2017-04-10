273e30533ab560d422ba7af0414d69ce46487b62
Magnus Hellman <magnus.hellman@gmail.com>
2017-04-10 09:47:27 +0200

    WIP

--- scripts/babel_prod.sh
+++ scripts/babel_prod.sh
@@ -2,4 +2,4 @@
 
 bin=$(readlink -f ./node_modules/.bin/browserify)
 
-$bin $@ -t uglifyify -t node-underscorify -t jstify -t babelify -t [ envify --NODE_ENV prod ] --no-bundle-external --bare
+$bin $@ -g uglifyify -t node-underscorify -t jstify -t babelify -t [ envify --NODE_ENV prod ] --no-bundle-external --bare
