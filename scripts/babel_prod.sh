28ad46875e29d754720569836dc9c3fa3bbe6428
Magnus Hellman <magnus.hellman@gmail.com>
2017-04-27 11:19:03 +0200

    cleanup

--- scripts/babel_prod.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-bin=$(readlink -f ./node_modules/.bin/browserify)
-
-$bin $@ -g uglifyify -t node-underscorify -t jstify -t babelify -t [ envify --NODE_ENV prod ] --no-bundle-external --bare
